eac6f7c68c5fa7e1eef1304bc289cc6ce7281f4d
Banjoko Oluwaseun <seunbanjoko2005@yahoo.com>
2024-12-12 09:41:59 +0000

    Link Product with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products, foreign_key: :seller_id, dependent: :destroy
 end
